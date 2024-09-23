89c38847f1f1604810c58c771a7cf9c230aeb850
thais-fujissaka <80991615+thais-fujissaka@users.noreply.github.com>
2024-09-22 22:25:28 -0300

    feat: adiciona busca por categoria, whey protein e creatina. Também adiciona o ProdutoCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,59 @@
 'use client'
 
 import Image from "next/image";
+import { IoMdSearch } from "react-icons/io";
+import { useRouter } from "next/navigation";
 import { useState } from 'react';
 
 const Homepage: React.FC = () => {
-  const [search, setSearch] = useState<string>('');
+	const [search, setSearch] = useState<string>('');
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
+	const router = useRouter();
 
+	// Função para lidar com a mudança no input de busca
+	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value);
+	};
+
+	// Função para redirecionar o usuário para a página de resultados
+	const handleSubmit = () => {
+		if (search) {
+			router.push(`/resultados?categoria=${search}`);
+		}
+	};
+
+	// Função para lidar com a tecla pressionada
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === 'Enter') {
+			handleSubmit();
+		}
+	};
+	
 	return (
-		<div className="bg-branco flex justify-center items-center mt-12">
-      		<div className="relative">
+		<div className="bg-branco flex justify-center mt-12">
+      		<div className="">
+				
 				<div>
 					<a><Image src="/logo.svg" alt="Supplipédia" width={458} height={132}></Image></a>
 				</div>
 
-				{/* Slogan */}
-				<p className="flex justify-center top-8">O seu guia de suplementação inteligente.</p>
-
 				{/* Barra de busca */}
-				<div>
+				<div className="flex justify-between border border-preto rounded-full px-4 py-1 mt-8">
 					<input 
 						type="text" 
 						placeholder="Buscar suplemento..." 
 						value={search} 
-						onChange={handleSearch} 
+						onChange={handleSearch}
+						onKeyDown={handleKeyDown} // Adicionando o evento de tecla
+						className="w-full focus:outline-none"
 					/>
-					<button onClick={() => alert(`Buscando ${search}`)}>Buscar</button>
+					<button onClick={handleSubmit}><IoMdSearch /></button>
 				</div>
+
+				{/*Botões*/}
+				
+				
+				<p className="flex justify-center mt-12">O seu guia de suplementação inteligente.</p>
 			</div>
     	</div>
   	);
--- src/app/resultados/page.tsx
+++ src/app/resultados/page.tsx
@@ -1,9 +1,46 @@
 'use client'
 
-export default function Resultado() {
+import { useSearchParams } from "next/navigation"
+import { useState, useEffect } from "react";
+import produtosMock from '../../../mock/produtos.json';
+import ProdutoCard from "../components/ProdutoCard"
+
+const Resultados: React.FC = () => {
+  const searchParams = useSearchParams();
+  const categoria = searchParams.get("categoria"); // Capturar a categoria da query string
+
+  // Filtrar os produtos pela categoria
+  const produtosFiltrados = produtosMock.filter(produto => produto.categoria.toLowerCase() === categoria?.toLowerCase());
+
   return (
-    <div className="bg-branco">
+    <section className="container mx-auto mt-12">
+      <h1 className="text-2xl font-bold">Resultados da Pesquisa</h1>
       
-    </div>
+      {/* Exibindo a categoria pesquisada */}
+      <p className="mt-4">Você pesquisou por: <strong>{categoria}</strong></p>
+
+      {/* Mapeando e exibindo os produtos filtrados */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+        {produtosFiltrados.length > 0 ? (
+          produtosFiltrados.map(produto => (
+            <ProdutoCard
+              key={produto.id_produto}
+              nome={produto.nome}
+              marca={produto.marca}
+              categoria={produto.categoria}
+              preco={produto.preco}
+              descricao={produto.descricao}
+              imagem_url={produto.imagem_url}
+              url={produto.url}
+              atributos={produto.atributos}
+            />
+          ))
+        ) : (
+          <p>Nenhum produto encontrado para a categoria {categoria}.</p>
+        )}
+      </div>
+    </section>
   );
-}
+};
+
+export default Resultados;
